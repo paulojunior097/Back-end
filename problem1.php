ac6c228a77999e26fff2f3da859bb9e2dfdf5053
paulojunior097 <60434024+paulojunior097@users.noreply.github.com>
2020-01-30 21:13:59 -0300

    Update and rename README.md to problem1.php

---
+++ problem1.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> fill form </title>
+</head>
+<body>
+<form method="POST" action="first.php">
+	<p>Fill in your information</p>
+<label>Name:</label><input type="text" name="Name" id="Name"><br>
+<label>E-mail:</label><input type="E-mail" name="E-mail" id="E-mail"><br>
+<label>Twitter:</label><input type="Twitter" name="Twitter" id="Twitter"><br>
+<input type="submit" value="Enviar" id="Enviar" name="Enviar">
+</form>
+</body>
+</html>
